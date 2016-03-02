3ee5bc3c0abf24d6d02703278505c09de1d7cf74
Anthony J. Thibault <tony@highfidelity.io>
2016-03-02 12:04:57 -0800

    first version

---
+++ README.md
@@ -0,0 +1,6 @@
+Shock
+------------
+
+asset list
+
+file:///msys64/home/anthony/code/shock/cargo-container.fbx
---
+++ backup-svo.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.call(["cp", "c:/Users/anthony/AppData/Roaming/High Fidelity/assignment-client/entities/models.json.gz", "."])
